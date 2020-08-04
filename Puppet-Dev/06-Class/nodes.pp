352e97375d0cf49e2a6797d256453722b5dc65af
root <root@server101.example.com>
2020-08-04 06:25:39 -0400

    Puppet-Dev

--- Puppet-Dev/06-Class/nodes.pp
+++ Puppet-Dev/06-Class/nodes.pp
@@ -1,16 +1,40 @@
+node 'server101.example.com' {
+  class {'motd': }
+  class {'info': }
+  class {'ntp': }
+}
+
+
+node 'server102.example.com' {
+  class {'info': }
+}
+
+
+
+class motd {
+  file { '/etc/motd':
+    ensure => 'present',
+    content => 'Host is Managed by Foreman Puppet Server',
+  }
+}
+
+
+class info { 
+
+  file { '/tmp/info.txt':
+    ensure => 'present',
+    content => inline_template("Created by Puppet Class at <%= Time.now %>\n")
+  }
+
+}
+
+
+class ntp {
+
 $ntpservice = $osfamily ? {
   'redhat' => 'ntpd',
   'debain' => 'ntp',
   defualt  => 'ntp',
-  }
-
-node 'server101.example.com' {
-
-
-
-  file { '/tmp/info.txt':
-    ensure => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n")
   }
 
   package { 'ntp':
@@ -21,7 +45,9 @@ node 'server101.example.com' {
    ensure => 'running',
    enable => 'true',
   }
+
 }
+
 
 
 node 'node105.example.com' {
